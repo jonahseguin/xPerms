14cf970622ac755902a5e73d23b22ec27f7680db
Shawckz <shawckz419@gmail.com>
2016-08-09 18:28:42 -0600

    Add AddGroup command

--- src/main/java/com/shawckz/xperms/commands/CmdAddGroup.java
+++ src/main/java/com/shawckz/xperms/commands/CmdAddGroup.java
@@ -1,5 +1,54 @@
 package com.shawckz.xperms.commands;
 
-public class CmdAddGroup {
+import com.shawckz.xperms.XPerms;
+import com.shawckz.xperms.command.GCmd;
+import com.shawckz.xperms.command.GCmdArgs;
+import com.shawckz.xperms.command.GCommand;
+import com.shawckz.xperms.permissions.PermServer;
+import com.shawckz.xperms.permissions.groups.Group;
+import com.shawckz.xperms.profile.XProfile;
+import org.bukkit.ChatColor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.scheduler.BukkitRunnable;
+
+public class CmdAddGroup implements GCommand{
+
+    @GCmd(name = "xperms addgroup", aliases = {"perms addgroup"}, permission = "xperms.addgroup",
+            usage = "/xperms addgroup <player> <server> <groups>", minArgs = 3)
+    public void onCmdAddGroup(final GCmdArgs args) {
+        final CommandSender sender = args.getSender().getCommandSender();
+
+        new BukkitRunnable(){
+            @Override
+            public void run() {
+                XProfile profile = XPerms.getInstance().getCache().lookupProfile(args.getArg(0));
+                if(profile != null) {
+                    String permServerString = args.getArg(1);
+                    if(XPerms.getInstance().getServerCache().hasPermServer(permServerString)) {
+                        PermServer permServer = XPerms.getInstance().getServerCache().getPermServer(permServerString);
+                        Group group = XPerms.getInstance().getGroupManager().getGroup(permServer, args.getArg(2));
+                        if(!profile.getGroups().getGroupSet(permServer).hasGroup(group)) {
+                            profile.getGroups().getGroupSet(permServer).saveGroup(group);
+                            profile.refreshPermissions();
+                            profile.update();
+                            sender.sendMessage(ChatColor.GREEN + "Added group "+group.getName()+" to " + profile.getName()
+                                    + " for server " + permServer.getName() + ".");
+                        }
+                        else{
+                            sender.sendMessage(ChatColor.RED + "That player already has that group for that server.");
+                        }
+                    }
+                    else{
+                        sender.sendMessage(ChatColor.RED + "Perm Server not found in cache.");
+                    }
+                }
+                else{
+                    sender.sendMessage(ChatColor.RED + "Player not found in database.");
+                }
+            }
+        }.runTaskAsynchronously(XPerms.getInstance());
+
+
+    }
 
 }
